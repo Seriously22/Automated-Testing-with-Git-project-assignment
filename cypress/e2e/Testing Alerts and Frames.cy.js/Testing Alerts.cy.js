d4b3996efe88532a7868c0ca6a0bb53f22621205
Rachelle Hunter <hntr.rchll@gmail.com>
2024-02-07 19:36:19 -0500

    Adding project to existing repository

--- cypress/e2e/Testing Alerts and Frames.cy.js/Testing Alerts.cy.js
+++ cypress/e2e/Testing Alerts and Frames.cy.js/Testing Alerts.cy.js
@@ -1,7 +1,28 @@
 const url = 'https://demoqa.com/alerts';
 
 
-describe('Testing alerts' , () => {
-    it('blocks alerts' , () => {
-   
+describe('Testing alert' , function () => {
+    // test case
+    it('blocks alerts' , function () => {
+    // launch the url
 cy.visit(url);
+
+       // click on submit button to produce the alert pop up
+//       cy.get('input[type="submit"]').click();
+
+describe('Testing timer alert' , function () => {
+    it('')
+    //launch the url
+    cy.visit(url);
+    // click on submit button to produce the alert pop up
+//       cy.get('input[type="submit"]').click();
+
+describe('Confirm alert' , function () => {
+        // test case
+        it('blocks alerts' , function () => {
+        // launch the url
+    cy.visit(url);
+    // click on submit button to produce the alert pop up
+//       cy.get('input[type="submit"]').click();
+    });
+ });
