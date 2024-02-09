bb5a7cbc77b33e314a497c15a509429cab167169
Rachelle Hunter <hntr.rchll@gmail.com>
2024-02-09 15:53:11 -0500

    Adding more code

--- cypress/e2e/Testing Alerts and Frames/Testing Alerts.cy.js
+++ cypress/e2e/Testing Alerts and Frames/Testing Alerts.cy.js
@@ -3,27 +3,26 @@ const url = 'https://demoqa.com/alerts';
 
 describe('Testing alert' , function () => {
     // test case
-    it('blocks alerts' , function () => {
+      it('should block alerts' , function () => {
     // launch the url
 cy.visit(url);
-
-       // click on submit button to produce the alert pop up
-//       cy.get('input[type="submit"]').click();
+    // click on submit button to produce the alert pop up
+    // cy.get('input[type="submit"]').click();
 
 describe('Testing timer alert' , function () => {
-    it('')
+      it('should'              , function () => {                           
     //launch the url
     cy.visit(url);
     // click on submit button to produce the alert pop up
-//       cy.get('input[type="submit"]').click();
+    // cy.get('input[type="submit"]').click();
 })
 describe('Confirm alert' , function () => {
-        // test case
-        it('blocks alerts' , function () => {
-        // launch the url
+    // test case
+      it('should'        , function () => {
+    // launch the url
     cy.visit(url);
     // click on submit button to produce the alert pop up
-//       cy.get('input[type="submit"]').click();
+    // cy.get('input[type="submit"]').click();
     });
  });
 })
