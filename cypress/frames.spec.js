026c9c8a23e33680b778cc48c8e996339c1981b1
Rachelle Hunter <hntr.rchll@gmail.com>
2024-02-11 13:57:12 -0500

    Second commit

--- cypress/frames.spec.js
+++ cypress/frames.spec.js
@@ -1,28 +1,29 @@
 /// <reference types = "Cypress"/>
-/// <reference types = "Cypress-iframe"/>  //ERROR MSG STATING "CANNOT FINE THE MEANING..."
+/// <reference types = "Cypress-iframe"/>
 import 'cypress-iframe'
 
 const url = 'https://demoqa.com/frames';
-
-
 
 describe('Handling Frames in Cypress', function () {
 
     it('single frame', function () {   // test case
         cy.visit(url);
-        cy.frameLoaded('#frame1');  // main webpage
-        cy.iframe().find('#sampleHeading').then(function(txt){  // gets hold of embedded webpage
-            const txtframe = txt.text();  
-            expect(txtframe).to.contains('');  // assertion to verify text
-            cy.log(txtframe);
+        cy.frameLoaded('#sampleHeading');  // main webpage
+        cy.iframe().find('#sampleHeading').then(function(txt){  // get embedded frame
+            const txtValue = txt.text();    //get text within it //return value of stored text
+            expect(txtValue).to.contains('This is a sample page');  // assertion to verify text
+            cy.log(txtValue);
         })
     })        
         
-    it('nested frame', function () {   // test case
+    it('nested frame', function () {
         cy.visit(url);
-        cy.get('#alertButton').click();
-        cy.on('window:alert', (alertText) => {   // trigger alert message
-        expect(alertText).to.contains('You clicked a button');  // assertion
+        //no locators provided
+        cy.frameLoaded('this site can't be reached');
+        cy.iframe().find('this site can't be reached').then(function(txt){  // gets hold of embedded webpage
+            const txtframe = txt.text();  
+            expect(txtframe).to.contains('');  // assertion to verify text
+            cy.log(txtframe);
         })
     })
 })
