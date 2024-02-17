ed1f1ff36f0036d16eca3293168cf318061b8fc6
Rachelle Hunter <hntr.rchll@gmail.com>
2024-02-16 19:40:11 -0500

    Second commit

--- cypress/e2e/Custom Command/customCommandTestFile.js
+++ cypress/e2e/Custom Command/customCommandTestFile.js
@@ -1,4 +1,35 @@
-Cypress.Commands.add(login);
+describe('Website login, navigation, and check out cart' , ()=> {
+})
+    it('should navigate to website, log in, search for products, go to checkout cart' , () => {
+        })
+    Cypress.Commands.add(login , (username, password) => {
+
+})
+
+//Validation of successful login        
+if (cy.url() === "https://www.saucedemo.com/inventory.html") {
+    cy.type(successfulLogin);
+
+} else {
+    console.log("Try Again");
+}
+//Quantity Selection Box in the Checkout Cart
+cy.get('.cart_quantity_label').click(); {
+
+}
+//Typo error on Products page
+cy.get('inventory_item_desc'); {
+
+}   
+//Lack of Navigation Options on the About page
+//Select hamburger menu, click on About
+cy.get('#react_burger_menu_btn').click(); {
+}
+cy.get('#about_sidebar_link').click(); {
+}
+
+
+
 
 
     
