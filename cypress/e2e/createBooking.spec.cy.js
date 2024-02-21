5367262442e4e1d2a2e54626c094cec72d24718b
Rachelle Hunter <hntr.rchll@gmail.com>
2024-02-21 15:30:14 -0500

    Second commit

--- cypress/e2e/createBooking.spec.cy.js
+++ cypress/e2e/createBooking.spec.cy.js
@@ -1,17 +1,25 @@
-describe('complete', () => {
+describe('Create Booking for API', () => {
+    it('POST booking information', () => {
+        cy.request('POST', 'https://restful-booker.herokuapp.com/booking')
+          '{
+        "firstname" : "Jim",  //with or w/o "" for the title???
+        "lastname" : "Brown",
+        "totalprice" : 111,
+        "depositpaid" : true,
+        "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+},
+    additionalneeds: "Breakfast"
+}'
 
-    it('complete', () => {
-  
-      cy.request('POST', 'https://restful-booker.herokuapp.com/booking')
-  
         .should((response) => {
   
           expect(response.status).to.eq(200);
   
           expect(response.body).to.have.length.greaterThan(0);
   
-        });
+        }
+      }
+    }
   
-    });
-  
-  });
