137eaeab2543cac1520a740d78b0e2b8016ed0d1
Rachelle Hunter <hntr.rchll@gmail.com>
2024-02-21 16:25:20 -0500

    First commit

---
+++ cypress/e2e/deteteBooking.cy.js
@@ -0,0 +1,12 @@
+describe('Delete Booking for API', () => {
+    it('DELETE for deleting booking information', () => {
+        cy.request('DELETE', 'https://restful-booker.herokuapp.com/booking/1')
+          .should((response) => {
+  
+    expect(response.status).to.eq(200);
+
+    expect(response.body).to.have.length.greaterThan(0);
+
+  });
+});
+})
