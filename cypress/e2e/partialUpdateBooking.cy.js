702456ebda1d45b51c32a27e4858640f980b55fa
Rachelle Hunter <hntr.rchll@gmail.com>
2024-02-21 16:31:55 -0500

    First commit

---
+++ cypress/e2e/partialUpdateBooking.cy.js
@@ -0,0 +1,16 @@
+describe('Partially update Booking information for API', () => {
+    it('PATCH for updating booking information', () => {
+        cy.request('PATCH', 'https://restful-booker.herokuapp.com/booking/:id')
+          {            
+        "firstname" : "James",
+        "lastname" : "Brown"
+    }
+.should((response) => {
+  
+    expect(response.status).to.eq(200);
+
+    expect(response.body).to.have.length.greaterThan(0);
+
+  });
+});
+});
