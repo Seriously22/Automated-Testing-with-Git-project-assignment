dfc4170ad8e7d37b70a6634a4e7f0d72daa37944
Rachelle Hunter <hntr.rchll@gmail.com>
2024-02-27 10:36:58 -0500

    Added notes about errors

--- cypress/e2e/partialUpdateBooking.cy.js
+++ cypress/e2e/partialUpdateBooking.cy.js
@@ -1,12 +1,16 @@
 describe('Partially update Booking information for API', () => {
     it('PATCH for updating booking information', () => {
-     cy.request('PATCH', 'https://restful-booker.herokuapp.com/booking/1', //change id 1 person to...
+     cy.request('PATCH', 'https://restful-booker.herokuapp.com/booking/550', //change id of John Smith to...
       {
         "firstname" : "James",
         "lastname" : "Brown"
         }) .should((response) => {
           expect(response.status).to.eq(200);
-          expect(response.body).to.have.length.greaterThan(0); //error: Status 403 Forbidden
-    });
-});
+          expect(response.body).to.have.length.greaterThan(0);
+        //created token in Postman
+        //error: Status 403 Forbidden in Cypress
+        //Forbiden in Postman
+    }); //per instructor, status code for PATCH is not '200', but Cypress states failure due to
+        //status code not being 2xx or 3xx.
+        });
 })
