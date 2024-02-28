6a0d1f74e386d5912643afc7b77545ac5f63b9bd
Rachelle Hunter <hntr.rchll@gmail.com>
2024-02-28 15:54:50 -0500

    noted error code

--- cypress/e2e/partialUpdateBooking.cy.js
+++ cypress/e2e/partialUpdateBooking.cy.js
@@ -15,12 +15,11 @@ describe('Partially update Booking information for API', () => {
         }) .should((response) => {
           expect(response.status).to.eq(200);
           expect(response.body).to.have.length.greaterThan(0);
-        //created token in Postman
+        //created token in Postman - error - Forbidden
         //error: Status 403 Forbidden in Cypress
-        //Forbiden in Postman
         //Failure on status code (200); per instructor, status code for PATCH is not '200', 
         //but Cypress states failure due to status code not being 2xx or 3xx.
         });
         });
         });
-        });
+        });//error Status 404 not found, failedOnStatusCode
